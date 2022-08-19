7f93cdc433263f11bec404cd8020bd28a3b3c7f6
Pete Shaw <lozlow@users.noreply.github.com>
2022-08-19 14:54:21 +0100

    Update url

--- dist/index.js
+++ dist/index.js
@@ -41,7 +41,7 @@ if (require.main === require.cache[eval('__filename')]) {
 /***/ ((module) => {
 
 module.exports.getDownloadUrl = function ({ kapitanVersion, pythonVersion }) {
-  const url = `https://github.com/humaans/setup-kapitan-action/blob/master/bin/kapitan-${kapitanVersion}-py${pythonVersion}.pex`
+  const url = `https://github.com/humaans/setup-kapitan-action/raw/master/bin/kapitan-${kapitanVersion}-py${pythonVersion}.pex`
 
   return url
 }
